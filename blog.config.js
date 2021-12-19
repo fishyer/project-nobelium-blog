2f20a2d938f0b4f243741612b7d317c1ed58d93e
Issei <ttsw099@gmail.com>
2021-12-20 00:38:47 +0900

    add googleSiteVerification

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   socialLink: 'https://twitter.com/Lyicheng',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+    googleSiteVerification: 'EOyOIBvRPl1U1dXPNPVTTCfBLDEf6Kty9iOCtzPDKDM' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
