d3f4d231a7c307294af722842b432a75f220a546
Issei <ttsw099@gmail.com>
2021-12-23 22:39:52 +0900

    change  menu name

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const NavBar = () => {
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
-    { id: 4, name: locale.NAV.FRIENDS, to: '/friends', show: true }
+    { id: 4, name: '友链', to: '/friends', show: true }
   ]
   return (
     <div className="flex-shrink-0">
