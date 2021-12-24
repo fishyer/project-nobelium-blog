4c0cfe9a46edf6cd4c6a216fcf4ebca8971da269
Issei <ttsw099@gmail.com>
2021-12-24 21:36:06 +0900

    add PORTFOLIO menu

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,8 @@ const NavBar = () => {
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
     { id: 3, name: locale.NAV.FRIENDS, to: '/friends', show: true },
-    { id: 4, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 4, name: locale.NAV.PORTFOLIO, to: 'http://ryouissei.com/', show: true },
+    { id: 5, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
